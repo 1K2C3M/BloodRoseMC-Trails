2d4d2a4e75e84222c1d7e5a604b17a2083019d2d
1K2C3M <62569063+1K2C3M@users.noreply.github.com>
2020-05-19 14:09:16 -0400

    New Enchantment Trail

--- src/me/Corey/trailsgui/models/Effects.java
+++ src/me/Corey/trailsgui/models/Effects.java
@@ -67,4 +67,30 @@ public class Effects {
 				
 			}, 0, 1);
 	}
+		
+		public void startEnchant() {
+			taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.getPlugin(Main.class), new Runnable() {
+			
+				double var = 0;
+				Location loc, first, second;
+				ParticleData particle = new ParticleData(player.getUniqueId());
+				
+				@Override
+				public void run() {
+					if (!particle.hasID()) {
+						particle.setID(taskID);
+					}
+					
+					var += Math.PI;
+					
+					loc = player.getLocation();
+					first = loc.clone().add(Math.cos(1.55), Math.sin(var) + 1, Math.sin(var));
+					second = loc.clone().add(Math.cos(1.55), Math.sin(var) + 1, Math.sin(var + Math.PI));
+					
+					player.getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, first, 1);
+					player.getWorld().spawnParticle(Particle.END_ROD, second, 0);
+				}
+				
+			}, 0, 1);
+	}
 }
